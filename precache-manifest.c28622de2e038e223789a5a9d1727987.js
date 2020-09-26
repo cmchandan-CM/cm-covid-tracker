0cf9506980aed426ab8601fbd1fe0ca643d57baa
Chandan Kashyap <55576520+cmchandan-CM@users.noreply.github.com>
2020-09-26 19:44:36 +0530

    Updates

---
+++ precache-manifest.c28622de2e038e223789a5a9d1727987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dc3622c1110411fbf709f0369024cd",
+    "url": "/cm-covid-tracker/index.html"
+  },
+  {
+    "revision": "5af4afa88b516c8a2398",
+    "url": "/cm-covid-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "bc7c0ca2751e0f8fc908",
+    "url": "/cm-covid-tracker/static/css/main.cbea24f3.chunk.css"
+  },
+  {
+    "revision": "5af4afa88b516c8a2398",
+    "url": "/cm-covid-tracker/static/js/2.e07b3d1f.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/cm-covid-tracker/static/js/2.e07b3d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7c0ca2751e0f8fc908",
+    "url": "/cm-covid-tracker/static/js/main.824d2edd.chunk.js"
+  },
+  {
+    "revision": "2fefd408e13d9d8ad1e2",
+    "url": "/cm-covid-tracker/static/js/runtime-main.dbbf3fc9.js"
+  }
+]);
